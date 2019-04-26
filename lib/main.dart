535797c97e66659c1aa0534823ac2f7d6c46086f
Eric Tavares <ericmartial.tavares@gmail.com>
2019-04-26 10:45:50 +0000

    Added new form to create product and add it to product list pages

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String, String>> _products = [];
+  List<Map<String, dynamic>> _products = [];
 
-  void _addProduct(Map<String, String> product) {
+  void _addProduct(Map<String, dynamic> product) {
     setState(() {
       _products.add(product);
     });
@@ -33,13 +33,12 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
           accentColor: Colors.deepOrange,
           primarySwatch: Colors.deepOrange),
-      // home: AuthPage(),
       routes: {
-        '/': (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct),
-        '/admin': (BuildContext context) => ProductsAdminPage()
+        '/': (BuildContext context) => ProductsPage(_products),
+        '/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct)
       },
       onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
@@ -61,7 +60,7 @@ class _MyAppState extends State<MyApp> {
       },
       onUnknownRoute: (RouteSettings settings) {
         return MaterialPageRoute(
-          builder: (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct)
+          builder: (BuildContext context) => ProductsPage(_products)
         );
       },
     );
--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  final Function addProduct;
+
+  const ProductCreatePage(this.addProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreateState();
+  }
+}
+
+class _ProductCreateState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String description = '';
+  double price = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: RaisedButton(
-      child: Text("Create"),
-      onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return Center(
-              child: Text("Some text"),
-            );
-          }
-        );
-      },
-    ),);
+    return Container(
+        margin: EdgeInsets.all(50.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Title'),
+              onChanged: (String value) {
+                setState(() {
+                  titleValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Description'),
+              maxLines: 5,
+              keyboardType: TextInputType.multiline,
+              onChanged: (String value) {
+                setState(() {
+                  description = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onChanged: (String value) {
+                setState(() {
+                  price = double.parse(value);
+                });
+              },
+            ),
+            RaisedButton(
+              child: Text('Save'),
+              onPressed: () {
+                final Map<String, dynamic> product = {
+                  'title': titleValue,
+                  'description': description,
+                  'price': price,
+                  'imageUrl': 'assets/some_food.jpg'
+                };
+                widget.addProduct(product);
+              },
+            )
+          ],
+        ));
   }
 }
